08d17923ff4a898d6f8cd992b5b8dba539f29c16
khpproud <proudkhp1@gmail.com>
2020-10-27 01:09:59 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+// Constants
+const PORT = process.env.PORT || 3000;
+
+// App
+const app = express();
+app.use(express.json());
+const productRoutes = require("./routes");
+const monggose = require("mongoose");
+
+monggose
+  .connect(
+    `mongodb+srv://peter:<password>@studying.nmbi9.mongodb.net/Studying?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDb connected..."))
+  .catch((e) => console.error(e));
+
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  console.log("Hello world");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on port: $${PORT}`)
+);
+
+module.exports = app;
+module.exports.server = server;
